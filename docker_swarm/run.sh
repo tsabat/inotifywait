81c228d92aa3d0f73770c3649458ef7b35a3223a
Laura Frank <ljfrank@gmail.com>
2016-08-04 12:49:34 +0200

    Updating bind-propagation to fix error with directory permissions

--- docker_swarm/run.sh
+++ docker_swarm/run.sh
@@ -4,14 +4,16 @@ docker build -t inotifywait .
 
 docker service create \
   --name docker_swarm \
-  --mount "type=bind,source=$(pwd)/watch,target=/opt/watch,bind-propagation=shared,writable=true" \
+  --mount type=bind,source=$(pwd)/watch,target=/opt/watch,bind-propagation="" \
   inotifywait
 
+for i in {1..10}; do echo "sleeping $i of 5"; sleep 1; done
 for i in {1..10}; do echo $i > watch/$i.txt; done
 
-sleep 5
 
 docker ps --filter name=docker_swarm --format={{.Names}} | xargs docker logs
+#docker ps --filter name=docker_swarm --format={{.Names}} | xargs docker logs -f
+
 rm watch/*txt
-#docker service rm docker_swarm
+docker service rm docker_swarm
 
