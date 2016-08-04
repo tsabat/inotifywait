032578024b093da73c98b447f5dd54dddeb4c556
Laura Frank <ljfrank@gmail.com>
2016-08-04 13:05:59 +0200

    Making sleep output consistent

--- docker_run/run.sh
+++ docker_run/run.sh
@@ -6,8 +6,7 @@ docker build -t inotifywait .
 docker run -d --name=docker_run -v $(pwd)/watch:/opt/watch inotifywait
 
 for i in {1..10}; do echo $i > watch/$i.txt; done
-
-sleep 5
+for i in {1..10}; do echo "sleeping $i of 10"; sleep 1; done
 
 #if you want to hack around, use this log command instead and remove the rm command at the end
 #docker logs -f docker_run
--- docker_swarm/run.sh
+++ docker_swarm/run.sh
@@ -7,9 +7,8 @@ docker service create \
   --mount type=bind,source=$(pwd)/watch,target=/opt/watch,bind-propagation="" \
   inotifywait
 
-for i in {1..10}; do echo "sleeping $i of 5"; sleep 1; done
 for i in {1..10}; do echo $i > watch/$i.txt; done
-
+for i in {1..10}; do echo "sleeping $i of 10"; sleep 1; done
 
 docker ps --filter name=docker_swarm --format={{.Names}} | xargs docker logs
 #docker ps --filter name=docker_swarm --format={{.Names}} | xargs docker logs -f
