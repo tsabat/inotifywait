3ea5f46ff3de76bd42856a7bb71e2e34e7d722bd
Laura Frank <ljfrank@gmail.com>
2016-08-04 11:43:14 +0200

    Beefing up the run script a bit and updating inotifywatch output format in Dockerfile CMD

--- docker_run/run.sh
+++ docker_run/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-docker run --rm --name=docker_run -v $(pwd)/watch:/opt/watch timsabat/inotifywait &
+set -e
+
+docker build -t inotify .
+docker run -d --name=docker_run -v $(pwd)/watch:/opt/watch inotify
+
 for i in {1..10}; do echo $i > watch/$i.txt; done
+
+sleep 5
+
+#if you want to hack around, use this log command instead and remove the rm command at the end
+#docker logs -f docker_run
+docker logs docker_run
+rm watch/*txt
+docker rm -f docker_run
